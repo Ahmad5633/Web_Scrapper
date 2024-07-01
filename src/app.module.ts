8cd4f3f677cb8d0eb097e9cd9d44260df6dfd9fc
Ahmad5633 <ahmadhameed5633@gmail.com>
2024-07-01 11:54:12 +0500

    Env Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'; // Import ConfigModule from NestJS config package
 import { ScraperModule } from './scrapper/scrapper.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // Initialize the ConfigModule
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'Ahmad@5633',
-      database: 'scrapedData',
+      type: process.env.DB_TYPE as 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true', // Convert to boolean
     }),
     ScraperModule,
   ],
